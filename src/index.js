0f2501e2f33811c4342c4564315e30950fe6e0d0
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-04-15 14:15:42 -0300

    fix: correct minor bugs in the application

--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
-// dicinedo para el express que voy usar view engine
-app.set("view engine", "ejs");
-// definir la carpeta de esataticos css y img
-app.use(express.static("public"));
-
-app.get("/", (req, res) => {
+// Estou dizendo para o Express usar o EJS como View engine
+app.set('view engine','ejs');
+app.use(express.static('public'));
+// Body parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+// Rotas
+app.get("/",(req, res) => {
     res.render("index");
 });
 
-app.get("/preguntar", (req, res) => {
+app.get("/perguntar",(req, res) => {
     res.render("perguntar");
 })
 
-app.listen(8080, () => {
-  console.log("Server is running on port 8080");
+app.post("/salvarpergunta",(req, res) => {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send("Formulário recebido! titulo " + titulo + " " + " descricao " + descricao);
 });
+
+
+app.listen(8080,()=>{console.log("App rodando!");});
