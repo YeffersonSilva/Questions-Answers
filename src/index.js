1185bb9b998b9d54ca6cf3638eb9f12bbda5ad17
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-04-16 16:02:03 -0300

    feat: create 'pergunta' route

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const connection = require("./database/dataBase");
-const Pergunta= require("./database/Perguntas");
+const Pergunta = require("./database/Perguntas");
 
 // Database
 connection
@@ -15,41 +15,60 @@ connection
 
 
 // Estou dizendo para o Express usar o EJS como View engine
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 // Body parser
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
 // Rotas
 app.get("/", (req, res) => {
-  Pergunta.findAll({raw: true}).then(perguntas => {
+  Pergunta.findAll({
+    raw: true, order: [
+      ['id', 'DESC']
+
+    ]
+  }).then(perguntas => {
     res.render("index", {
       perguntas: perguntas
-    
+
     });
   }
-)
-    
+  )
+
 });
 
-app.get("/perguntar",(req, res) => {
-    res.render("perguntar");
+app.get("/perguntar", (req, res) => {
+  res.render("perguntar");
 })
 // recibe del formulario
 
-app.post("/salvarpergunta",(req, res) => {
-    var titulo = req.body.titulo;
-    var descricao = req.body.descricao;
+app.post("/salvarpergunta", (req, res) => {
+  var titulo = req.body.titulo;
+  var descricao = req.body.descricao;
 
-    Pergunta.create({
-        titulo: titulo,
-        descricao: descricao
-    }).then(() => {
-        res.redirect("/");
-    });
+  Pergunta.create({
+    titulo: titulo,
+    descricao: descricao
+  }).then(() => {
+    res.redirect("/");
+  });
 });
+// rota da pergunta
+app.get("/pergunta/:id", (req, res) => {
+  var id = req.params.id;
+  Pergunta.findOne({
+    where: { id: id }
+  }).then(pergunta => {
+    if (pergunta != undefined) {
+      res.render("pergunta", {
+        pergunta: pergunta
+      });
+    } else {
+      res.redirect("/");
+    }
+  });
+})
 
-
-app.listen(8080,()=>{console.log("App rodando!");});
+  app.listen(8080, () => { console.log("App rodando!"); });
